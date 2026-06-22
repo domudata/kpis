e3eaa8617a8caacd92286ecdfe85ab4e9e105c4e
domu data <domudata@gmail.com>
2026-06-23 00:03:30 +0100

    Fix formatting issues in ocp9.py

--- ocp9.py
+++ ocp9.py
@@ -1374,8 +1374,8 @@ def main():
 # ==============================================================
 # FONCTION POUR EXCLURE LES COMPRESSEURS
 # ==============================================================
-           dfp = excr(res['dfp'])  # Exclut les compresseurs des OT
-           avf = excr(res['avf'])  # Exclut les compresseurs des Avis
+            dfp = excr(res['dfp']) 
+            avf = excr(res['avf']) 
             ano_map = {}
             ano_map["TAUX_REALISATION_CORRECTIF/PT"] = dfp[(dfp["Nº appel pl.entret."].fillna(0)==0)&(dfp["Contient SOPL"]==1)&(~dfp["Statut OT"].isin(["CLOT","TCLO"]))].groupby("Poste travail princ.")["Ordre"].count()
             prep_filt = (dfp["Statut OT"]=="CRÉÉ")&(dfp["Statut utilisateur"].str.contains("CRPR",na=False))
